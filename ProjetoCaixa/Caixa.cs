e98bf2ed5b42ea95dc794ad65ca051dbe5300940
Abimael Santos <abymaelsantos@gmail.com>
2024-01-10 18:18:15 -0300

    Criando projeto Caixa com POO na Linguagem C#

---
+++ ProjetoCaixa/Caixa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoCaixa
+{
+    class Caixa
+    {
+        // Atributos
+        public double largura;
+        public double altura;
+        public double profundidade;
+
+        // Métodos
+        public double calcularVolume()
+        {
+            double volume = (largura * altura * profundidade);
+            return volume;
+        }
+    }
+}
---
+++ ProjetoCaixa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoCaixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caixa caixa1 = new Caixa();
+
+            Console.WriteLine("\n- Informe a Altura");
+            caixa1.altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n- Informe a Largura");
+            caixa1.largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n- Informe a Profundidade");
+            caixa1.profundidade = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("- Calculo do Volume = " + caixa1.calcularVolume());
+            Console.WriteLine("\nPressione a Tecla Enter para Sair...");
+            Console.ReadKey();
+        }
+    }
+}
